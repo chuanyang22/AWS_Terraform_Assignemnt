533081c3d1b22f22d21f2e52151fe1f203a57f16
isitharu <singweetie@gmail.com>
2026-08-24 01:50:39 +0800

    Debug config.php

--- src/index.php
+++ src/index.php
@@ -4,6 +4,7 @@ require 'auth.php';
 require 'helpers.php';
 
 $search = trim($_GET['q'] ?? '');
+die("DEBUG: host=" . $host . " user=" . $user . " pass=" . $pass . " dbname=" . $dbname);
 
 if ($search !== '') {
     $stmt = $conn->prepare('SELECT * FROM facilities WHERE name LIKE ? ORDER BY name');
