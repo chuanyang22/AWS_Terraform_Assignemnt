5a3afbd234841c83be2e3b865def9c541edb1099
isitharu <singweetie@gmail.com>
2026-08-23 23:10:12 +0800

    invalid ip range in variables.tf

--- infra/modules/vpc/variables.tf
+++ infra/modules/vpc/variables.tf
@@ -7,7 +7,7 @@ variable "name_prefix" {
 variable "vpc_cidr" {
   description = "CIDR block for the VPC."
   type        = string
-  default     = "172.168.0.0/16"
+  default     = "172.16.0.0/16"
 }
 
 variable "azs" {
